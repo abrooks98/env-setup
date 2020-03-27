7ae9b5c3af77ad1c95a4662fa634370f0bc3e6c5
Alex Brooks <alex.brooks@intel.com>
2020-03-27 13:53:46 -0500

    vim: install fugitive with setup

--- vim/setup.sh
+++ vim/setup.sh
@@ -9,4 +9,7 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 cp $DIR/.vimrc ~/.vimrc
 
+mkdir -p ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/fugitive.git ~/.vim/pack/tpope/start/fugitive
+
 echo "Finished setting up Vim"
